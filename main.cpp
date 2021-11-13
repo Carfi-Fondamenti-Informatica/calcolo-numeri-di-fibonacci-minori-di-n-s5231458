95c0baf516b578701cc5b3809901835944fcfed7
Marco Badin <91483735+s5231458@users.noreply.github.com>
2021-11-13 19:48:31 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-   return 0;
-}
+
+    int n=0;
+    cin>>n;
+
+    int a=0, b=1, c=a+b;
+    cout<<b<<endl;
+
+    while(c<=n){
+
+        c=a+b;
+        a=b;
+        b=c;
+        cout << a << endl;
+    }
